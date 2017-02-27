527e5df241a7a8ad10e1bfc3064326e9931581d8
whitequark <whitequark@whitequark.org>
2017-02-27 20:35:16 +0000

    Update the testsuite as camlp4 is no longer a part of OCaml.

--- testsuite/external.ml
+++ testsuite/external.ml
@@ -2,6 +2,23 @@
 #use "findlibonly_test_header.ml";;
 #use "external_test_header.ml";;
 
+let () = test "camlp4.opt"
+  ~description:"Fixes PR#5652"
+  ~options:[`package "camlp4.macro";`tags ["camlp4o.opt"; "syntax\\(camp4o\\)"];
+            `ppflag "camlp4o.opt"; `ppflag "-parser"; `ppflag "macro";
+            `ppflag "-DTEST"]
+  ~tree:[T.f "dummy.ml"
+            ~content:"IFDEF TEST THEN\nprint_endline \"Hello\";;\nENDIF;;"]
+  ~matching:[M.x "dummy.native" ~output:"Hello"]
+  ~targets:("dummy.native",[]) ();;
+
+let () = test "SyntaxFlag"
+  ~options:[`use_ocamlfind; `package "camlp4.macro"; `syntax "camlp4o"]
+  ~description:"-syntax for ocamlbuild"
+  ~tree:[T.f "dummy.ml" ~content:"IFDEF TEST THEN\nprint_endline \"Hello\";;\nENDIF;;"]
+  ~matching:[M.f "dummy.native"]
+  ~targets:("dummy.native",[]) ();;
+
 let () = test "SubtoolOptions"
   ~description:"Options that come from tags that needs to be spliced \
                 to the subtool invocation (PR#5763)"
