49e5830cb875eb785899c0e435a4e02e431dd855
Hugo Heuzard <hugo.heuzard@lambda-coins.com>
2023-12-20 15:47:54 +0100

    Fix compilation with ocaml-trunk

--- src/my_std.mli
+++ src/my_std.mli
@@ -42,7 +42,7 @@ module Digest : sig
   type t = string
   val string : string -> t
   val substring : string -> int -> int -> t
-  external channel : in_channel -> int -> t = "caml_md5_chan"
+  val channel : in_channel -> int -> t
   val file : string -> t
   val output : out_channel -> t -> unit
   val input : in_channel -> t
